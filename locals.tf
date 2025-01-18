8751475e00e901166a993f9663077b1fd551e405
hemanth <hemanthtadikonda29@gmail.com>
2025-01-18 17:11:14 +0530

    User-Info-app  Iac_code

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   vpc_id = lookup(lookup(module.vpc,"main",null), "vpc_id",null)
 
-  app_subnets = [ for k,v in lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnets",null),"app",null),"subnets_ids",null) : v.id ]
+  app_subnets = [ for k,v in lookup(lookup(lookup(lookup(lookup(module.vpc,"main",null ),"vpc",null)"subnets",null),"app",null),"subnets_ids",null) : v.id ]
 }
